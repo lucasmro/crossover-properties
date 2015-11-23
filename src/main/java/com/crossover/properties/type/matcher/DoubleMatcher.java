e20f632326af3c6ee1a4d4d3ab4099578e5aaebc
lucasmro <lucasmro@gmail.com>
2015-11-23 11:09:00 -0200

    Add DoubleMatcher and its test

---
+++ src/main/java/com/crossover/properties/type/matcher/DoubleMatcher.java
@@ -0,0 +1,16 @@
+package com.crossover.properties.type.matcher;
+
+import java.util.regex.Pattern;
+
+public class DoubleMatcher extends TypeMatcher {
+
+    @Override
+    protected Pattern getPattern() {
+        return Pattern.compile("[-+]?[0-9]*\\.[0-9]+([eE][-+]?[0-9]+)?");
+    }
+
+    @Override
+    protected String getName() {
+        return Double.class.getName();
+    }
+}
